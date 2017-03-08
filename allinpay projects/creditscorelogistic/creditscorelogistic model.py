2a4a1c1d8c0c90ccb1093aae0fbce2df12f08ba0
fengyz <fengyz@allinpay.com>
2017-03-08 11:37:15 +0800

    comment

--- allinpay projects/creditscorelogistic/creditscorelogistic model.py
+++ allinpay projects/creditscorelogistic/creditscorelogistic model.py
@@ -10,9 +10,11 @@ sys.path.append("allinpay projects")
 from creditscorelogistic.creditscorelogistic import CreditScoreLogistic
 
 
-##################
+##############################################################################
+##############################################################################
 #一，初始化模型数据
-#################
+##############################################################################
+##############################################################################
 
 dataname = 'HMEQ'
 #dataname = 'german'
@@ -21,9 +23,11 @@ dataname = 'HMEQ'
 logisticmodel = CreditScoreLogistic(dataname)
 self = logisticmodel
 
-#################
+##############################################################################
+##############################################################################
 #二，设置模型参数
-#################
+##############################################################################
+##############################################################################
 # WOE 分割数等份
 binn = 10
 # 测试样本大小
@@ -41,9 +45,11 @@ nclusters=10
 #粗分类时聚类的方法
 cmethod='kmeans'
 
-#################
+##############################################################################
+##############################################################################
 #三，建模并预测
-#################
+##############################################################################
+##############################################################################
 #####1，不筛选变量的完整模型
 feature_sel = "origin"
 #1)简单粗分类
@@ -61,34 +67,47 @@ predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, nclusters=nclust
 ######2，VarianceThreshold过滤变量
 feature_sel = "VarianceThreshold"
 varthreshold = 0.2
+#1)简单粗分类
 #单次的train and test
 predresult = self.logistic_trainandtest(binn, testsize, cv, feature_sel, varthreshold, bq=bq)
-
 #K重train and test
 predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, feature_sel, varthreshold, bq=bq)
 
+#2)聚类粗分类
+#单次的train and test
+predresult = self.logistic_trainandtest(binn, testsize, cv, feature_sel, varthreshold, bq=bq, nclusters=nclusters, cmethod=cmethod)
+#K重train and test
+predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, feature_sel, varthreshold, bq=bq, nclusters=nclusters, cmethod=cmethod)
+
 #####3，RFECV递归+CV选择变量
 feature_sel = "RFECV"
+#1)简单粗分类
 #单次的train and test
+predresult = self.logistic_trainandtest(binn, testsize, cv, feature_sel, bq=bq)
+#K重train and test
+predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, feature_sel, bq=bq)
+
+#2)聚类粗分类
+#单次的train and test
+predresult = self.logistic_trainandtest(binn, testsize, cv, feature_sel, bq=bq, nclusters=nclusters, cmethod=cmethod)
+#K重train and test
+predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, feature_sel, bq=bq, nclusters=nclusters, cmethod=cmethod)
+
 
 # 遍历测试binn,binn从3到100，本方法已包括模型评估，并且保存到文件中
 predresult = self.looplogistic_trainandtest(testsize, cv, feature_sel, bq=bq)
-    
-# 传入binn
-predresult = self.logistic_trainandtest(binn, testsize, cv, feature_sel, bq=bq)
 
-#K重train and test
-
-# 遍历测试binn,binn从3到100，本方法已包括模型评估，并且保存到文件中
 predresult = self.looplogistic_trainandtest_kfold(nsplit, cv, feature_sel, bq=bq ,op=op)
 
 predresult = self.looplogistic_trainandtest_kfold_LRCV(nsplit, cv, feature_sel, bq=bq ,op=op)
     
-predresult = self.logistic_trainandtest_kfold(binn, nsplit, cv, feature_sel, bq=bq)
 
-#################
+
+##############################################################################
+##############################################################################
 #四，模型预测结果评估
-#################
+##############################################################################
+##############################################################################
 self.modelmetrics_scores(predresult)
 
 
