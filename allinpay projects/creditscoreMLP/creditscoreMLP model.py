a48feac16e29ce7c2807baed183a52b67b44555d
fengyz <fengyz@allinpay.com>
2017-03-09 14:02:14 +0800

    update model

--- allinpay projects/creditscoreMLP/creditscoreMLP model.py
+++ allinpay projects/creditscoreMLP/creditscoreMLP model.py
@@ -26,15 +26,7 @@ self = MLPmodel
 #二，设置模型参数
 ##############################################################################
 ##############################################################################
-testsize = 0.25
-hidden_layer_sizes = (64,)
-#hidden_layer_sizes = (64,32,)
-activation = 'relu'
-#activation = 'logistic'
-alpha = 0.0001
-nsplit = 5
-cv = 10
-varthreshold = 0.2
+#1,粗分类和woe转换设置
 #粗分类时聚类的数量
 nclusters=10
 #粗分类时聚类的方法,kmeans,DBSCAN,Birch，quantile(等分位数划分)，None(等距划分)
@@ -43,18 +35,35 @@ cmethod = 'quantile'
 #cmethod = 'kmeans'
 #cmethod = 'Birch'
 #method = 'DBSCAN'
+#2，训练集和测试集的划分
+# 测试样本大小
+testsize = 0.25
+#交叉检验法分割数量
+nsplit = 5
+#3，变量筛选设置
+feature_sel = 'origin'
+#feature_sel = "VarianceThreshold"
+#feature_sel = "RFECV"
+#feature_sel == "SelectFromModel"
+#feature_sel == "SelectKBest"
+cv = 10
+varthreshold = 0.2
+#4，MLP算法设置
+hidden_layer_sizes = (64,)
+#hidden_layer_sizes = (64,32,)
+activation = 'relu'
+#activation = 'logistic'
+alpha = 0.0001
 
 ##############################################################################
 ##############################################################################
 #三，建模并预测
 ##############################################################################
 ##############################################################################
-#1，不筛选变量的完整模型
-feature_sel = 'origin'
 #单次的train and test
-predresult = self.MLP_trainandtest(testsize, cv, feature_sel, varthreshold, activation, alpha, *hidden_layer_sizes, nclusters=nclusters, cmethod=cmethod)
+predresult = self.MLP_trainandtest(testsize, cv, feature_sel, varthreshold, activation, alpha, *hidden_layer_sizes, nclusters, cmethod)
 #K重train and test
-predresult = self.MLP_trainandtest_kfold(nsplit, cv, feature_sel, varthreshold, activation, alpha, *hidden_layer_sizes, nclusters=nclusters, cmethod=cmethod)
+predresult = self.MLP_trainandtest_kfold(nsplit, cv, feature_sel, varthreshold, activation, alpha, *hidden_layer_sizes, nclusters, cmethod)
 
 #2，VarianceThreshold过滤变量
 feature_sel = "VarianceThreshold"
